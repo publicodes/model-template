0d35334867cd40885d9c9d8ccc6d1d4c9468854c
Emile Rolley <emile.rolley@tuta.io>
2023-09-12 11:46:04 +0200

    feat(doc): add markdown support

--- doc/src/App.tsx
+++ doc/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { RulePage } from "publicodes-react";
 import { Link, Route, Routes, useParams } from "react-router-dom";
 import { ComponentProps, useRef } from "react";
+import ReactMardown from "react-markdown";
 
 import model from "./%PACKAGE_NAME%.model.json";
 
@@ -16,6 +17,7 @@ function Documentation() {
   const url = useParams()["*"];
   const { current: renderers } = useRef({
     Link,
+    Text: ({ children }) => <ReactMardown children={children} />,
   } as ComponentProps<typeof RulePage>["renderers"]);
 
   return (
